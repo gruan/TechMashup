2a8b25176b3a15a2d507919450fa523e541d1678
trueb2 <trueb2@illinois.edu>
2014-09-14 17:12:20 -0500

    Uploading this to server

--- homescript.js
+++ homescript.js
@@ -7,11 +7,10 @@
 
 var c0 = ["Generic Class", "Generic Link"];
 var classes = [c0]; //holds arrays of class info [[className0, link0, link1], [className1, link0, link1, etc.], etc.]
-var displayedClasses;
-var scratch = 2;
-var pageLoadinLinkCounter = 1;
-var classCount = 1;
-var loading = 1; //the page is not done loading while 1
+var d0 = [0, 0];
+// var displayedClasses = [d0];
+var loading = 1;
+var loadingCount = 1; 
 
 /*
 * when the page loads, this function is called
@@ -23,7 +22,6 @@ var loading = 1; //the page is not done loading while 1
 */
 function startup(){
 	var cookieish = localStorage.getItem("classes"); //like, but not a cookie
-	// localStorage.clear(); //used to delete stored data
 
 	if(cookieish == null){
 		loading = 0;
@@ -34,20 +32,20 @@ function startup(){
 	classes = JSON.parse(cookieish);
 
 	//remove initial row
-	var tr = document.getElementById("row1")
+	var tr = document.getElementById("row1");
 	tr.parentNode.removeChild(tr);
 
-	for(var i = 0; i < classes.length; i++){
-		var clss = [];
-		for(var j = 0; j < classes[i].length; i++){
-			clss.push(0);
-		}
-		displayedClasses[i] = clss;
-	}
-
 	//repopulate the table
+	populate();
+
+	loading = 0;
+}
+
+/*
+* populates the table using addClass() and addLink()
+*/
+function populate(){
 	var count = classes.length;
-	// alert("classes.length = " + count);
 	for(var i = 0; i < count; i++){
 		//add each class and its links to the table
 		var c = classes[i][0];
@@ -55,16 +53,153 @@ function startup(){
 		addClass(c); //add class
 		var linkcount = classes[i].length - 1;
 		for(var j = 1; j <= linkcount; j++){
-			var actualLink = JSON.stringify(classes[i][j]);
+			var link = JSON.stringify(classes[i][j]);
 			var cname = JSON.stringify(classes[i][0]);
-			actualLink = clean(actualLink);
+			link = clean(link);
 			cname = clean(cname);
-			// alert("cname is " + cname);
-			addLink(cname, actualLink); //add link
-			pageLoadinLinkCounter = 1;
+			if(link.substring(0,4) === "http"){
+				addLink(cname, link); //add link
+			}
 		}
 	}
-	loading = 0;
+}
+
+/*
+* adds a class, updating the page and the array 
+*/
+function addClass(className) {
+	if(loading == 0){
+		className = prompt("What is the name of the class you are trying to add?","Ex. RHET 105");
+	}
+
+	if(className == null){
+		return;
+	}
+
+	var class0 = classes[0][0];
+	if (class0 === "Generic Class") { //setting first class
+		document.getElementById("p1").innerHTML = className;
+
+		//keep the classes array updated
+		classes[0][0] = className;
+	} else {
+		//keep classes array updated
+		if(loading == 0){
+			var newClass = [className, "Click \"Add Link\""];
+			classes.push(newClass);
+		}
+		//add new row
+		var table = document.getElementById("maintable");
+		var row = table.insertRow(table.rows.length);
+
+		if(loading == 1){
+			row.id = "row" + loadingCount;
+		} else {
+			row.id = "row" + (classes.length); //may need to change if extra links are taking up more rows
+		}
+
+		//insert the new cells
+		var cell1 = row.insertCell(0);
+		var cell2 = row.insertCell(1);
+		var cell3 = row.insertCell(2);
+
+		//adding text and stuff
+		var t = classes.length;
+		if(loading == 1){
+			t = loadingCount;
+			loadingCount = loadingCount + 1;
+		}
+
+		pTag(cell1, t, className);
+		cbox(cell2, t);
+		aLink(cell3, t +"1", "abcdehijklmnopqrstuvwxyz", "Click \"Add Link\"");
+	}
+	save(); //update the cookie for the next time the user logs on
+}
+
+/*
+* adds a link either removing a click add link/general link
+* or adding a row beneath the corresponding class
+*/
+function addLink(className, link) {
+	if(loading == 0){ //not called on page load
+		className = prompt("To which class do you want to add another link?","Ex. RHET 105");
+	}
+	
+	if(className == null){ //no user input
+		return;
+	}
+
+	//in order to add we need to add a new row or replace a generic link
+	for(var i = 0; i < classes.length; i++){
+		if(classes[i][0] === className){
+			if(classes[i][1] === "Click \"Add Link\"" || classes[i][1] === "Generic Link" || (loading == 1 && classes[i][1] == link)){
+				//we do not need a new row change this link
+				l1 = document.getElementById("l"+(i+1)+"1", "");
+				linkIt(l1, link, i, 1);
+				save();
+				return;
+			}
+			var p = document.getElementById("p"+(i+1));
+
+			// alert(p.innerHTML);
+			if(p.innerHTML === className){
+				//add a row here with the content in classes[i][j]
+				var table = document.getElementById("maintable");
+				var row0 = p.parentNode.parentNode;
+				var row = table.insertRow(row0.rowIndex+1);
+				var cell1 = row.insertCell(0);
+				var cell2 = row.insertCell(1);
+				var cell3 = row.insertCell(2);
+				// pTag(cell1, "", "");
+				cbox(cell2, table.rows.length-1);
+				var linkElement =  aLink(cell3, (i+1)+""+classes[i].length, link, "Click \"Add Link\"");
+				linkIt(linkElement, link, i, classes[i].length);
+				for(var j = 0; j < classes[i].length; j++){
+					if(classes[i][j] === link){
+						linkElement.id = "l"+(i+1)+j;
+					}
+				}
+				save();
+				return;
+			}
+		}
+	}
+	save();
+}
+
+/*
+* takes a link element and gives it a name and an href
+*/
+function linkIt(element, link, classIndex, linkIndex){
+	var linkName = "";
+	if(loading == 0){
+		link = "";
+
+		link = prompt("What is a website that your class uses. The more exact the better, so it is recommended that you go to the actual page you visit most often and copy the address.","");
+
+		linkName = prompt("What would you like to call this link?");
+		if(linkName === ""){
+			linkName = link;
+		}
+		localStorage.setItem(link, linkName);
+	} else {
+		linkName = localStorage.getItem(link);
+	}
+
+	element.href = link;
+	element.innerHTML = linkName;
+
+	if(loading == 0){
+		var c = classes[classIndex];
+		if(linkIndex == c.length){
+			c.push(link);
+		} else {
+			c[linkIndex] = link;
+		}
+	}	
+
+	// displayed(link);
 }
 
 /*
@@ -76,201 +211,49 @@ function save(){
 		return;
 	}
 	var classesString = JSON.stringify(classes);
-	alert(classesString);
- 	// createCookie("cookie", "test", 30);
  	localStorage.setItem("classes", classesString);
-	// alert("Attempted to save");
 }
 
 /*
-* adds a class, updating the page and the array 
+* adds a paragraph to the parent passed with the content
+* passed
 */
-function addClass(className) {
-	var alreadyAdded = 1;
-	if(className === ""){
-		className = prompt("What is the name of the class you are trying to add?","Ex. RHET 105");
-		alreadyAdded = 0;
-	}
+function pTag(parent, id, content){
+	var table = document.getElementById("maintable");
+	var pCName = document.createElement("p");
+	pCName.id = "p" + id; //will be different when adding new rows for links
+	pCName.innerHTML = content;
+	parent.appendChild(pCName);
+	return pCName;
+}
 
-	var class1 = classes[0][0];
-
-	if(className == null){
-		return;
-	}
-
-	if (class1 === "Generic Class") { //setting first class
-		document.getElementById("p1").innerHTML = className;
-
-		classCount = classCount + 1;
-		//keep the classes array updated
-		classes[0][0] = className;
-		displayedClasses[0][0] = 1;
-	} else {
-		//add new row
-		var table = document.getElementById("maintable");
-		var row = table.insertRow(table.rows.length);
-		row.id = "row" + (table.rows.length - 1); //may need to change if extra links are taking up more rows
-
-		//insert the new cells
-		var cell1 = row.insertCell(0);
-		var cell2 = row.insertCell(1);
-		var cell3 = row.insertCell(2);
-		// 		var cell4 = row.insertCell(3);
-
-		//adding text and stuff
-		var pCName = document.createElement("p");
-		pCName.id = "p" + (table.rows.length - 1); //will be different when adding new rows for links
-		pCName.innerHTML = className;
-		cell1.appendChild(pCName);
-
+/*
+* adds a checkBox to the parent passed as a parameter and 
+* sets its attributes to the standard with the unique id
+* passed preceded by cd
+*/
+function cbox(parent, id){
 		var checkBox = document.createElement("input");
-		checkBox.id = "cb" + table.rows.length - 1;
+		checkBox.id = "cb" + id;
 		checkBox.type = "checkbox";
 		checkBox.style.cssFloat = "right";
-		cell2.appendChild(checkBox);
-
-		var link = document.createElement("a");
-		link.href = "abcdehijklmnopqrstuvwxyz";
-		link.innerHTML = "Click \"Add Link\"";
-		// alert("Class count " +classCount);
-		link.id = "l" + classCount + "1"; //l11 or l21 or l31 ... first number increments by number of classes second increments by number of links in that class
-		classCount = classCount + 1;
-		// alert("Class count " +classCount);
-		// alert("link.id = " + link.id);
-		cell3.appendChild(link);
-
-		//keep classes array updated
-		if(alreadyAdded == 0){
-			var newClass = [className, "Click \"Add Link\""];
-			classes.push(newClass);
-			displayedClasses.push([1, 0]);
-		}
-	}
-	save(); //update the cookie for the next time the user logs on
-}
-
-function addLink(className, actualLink) {
-	// 	alert("You are trying to add a link");
-	if(actualLink == "" || className == ""){ //not called on page load
-		className = prompt("To which class do you want to add another link?","Ex. RHET 105");
-	}
-	
-	if(className == null){ //no user input
-		return;
-	}
-
-	for (var i = 0; i < classes.length; i++) {
-		var name = classes[i][0]; //get the class name from the array
-		// var name = "\""+name+"\"";
-		// alert(name + " " + className);
-		if (name === className) {
-			var l1 = classes[i][1]; //check first link to see if it has been assigned
-			var link1 = document.getElementById("l"+(i+1)+"1");
-			// alert("before logic link1.innerHTML = " + link1.innerHTML + " and l1 = " + l1);
-			// if (l1 === "Click \"Add Link\"" || l1 === "Generic Link" || link1.innerHTML != l1) { //prompt for Link Name and hyperlink
-				if (l1 === "Click \"Add Link\"" || l1 === "Generic Link" || link1.href.substring(0, 5) != "http:") { //prompt for Link Name and hyperlink
-				// alert("linking generic link "+actualLink);
-				var keepGoing = linkIt(link1, i, 1, actualLink);
-
-				if(keepGoing === 0){
-					return;
-				}
-			} else { //find out which number link we need to add and prompt for link name and hyperlink
-				//any additional links will require additional rows to be added to the table beneath a class
-				//row will be inserted beneath last link already present in the class's group
-				var table = document.getElementById("maintable");
-				var t = 1;
-				for(var z = 0; z < i; z++){
-					t += classes[z].length;
-				}
-				t += classes[i].length - 1; //-1 for each not displayed
-
-				var row = table.insertRow(t);
-
-				row.id = "row" + scratch + "" + classes[i].length; //may need to change if extra links are taking up more rows
-
-				//insert the new cells
-				var cell1 = row.insertCell(0);
-				var cell2 = row.insertCell(1);
-				var cell3 = row.insertCell(2);
-
-				//check box and link 
-				var checkBox = document.createElement("input");
-				checkBox.id = "cb" + t;
-				checkBox.type = "checkbox";
-				checkBox.style.cssFloat = "right";
-				cell2.appendChild(checkBox);
-
-				// alert("linking link in new row " + actualLink);
-				var link = document.createElement("a");
-				linkIt(link, i, classes[i].length, actualLink); //find out after user input
-				link.id = "l" + scratch + "" + classes[i].length;
-				scratch = scratch + 1;
-				cell3.appendChild(link);
-			}
-		}
-	}
-
-	save(); //update the cookie for the next time that the user logs on
+		parent.appendChild(checkBox);
+		return checkBox;
 }
 
 /*
-* takes a link from addLink (<a href="http://www.website.com")
-* assigns the href of the link and the name that the user sees when
-* viewing the page
+* adds a <a> element to the passed parent with
+* the params added as attributes
 */
-function linkIt(link1, classIndex, linkIndex, link) {
-	var haveLinkName = 1;
-	// alert("good spot");
-
-	var length = link.length;
-	//link needs to be cleaned up
-	var cleaned = link.substring(1, link.length-1);
-	// alert("fixed = ? " + cleaned);
-
-	if(link === "Generic Link" || link === ""){ //this is not a page page reload
-		// alert("in here");
-		link = prompt("What is a website that your class uses. The more exact the better, so it is recommended that you go to the actual page you visit most often and copy the address.","");
-
-		if(link == null){ //tells add link to return before making extra rows
-			return 0;
-		}
-		haveLinkName = 0;
-	}
-
-
-	link1.href = link;
-
-	var linkName = "";
-
-	//if we do not already have the preferred name of the link ask
-	if(haveLinkName == 0){
-		linkName = prompt("What would you like to call this link?");
-		// alert(linkName);
-		if(linkName === ""){
-			linkName = link;
-		}
-		localStorage.setItem(link, linkName);
-	} else { //acquire the link name from storage
-		linkName = localStorage.getItem(link);
-	}
-
-	if (linkName == null) {
-		link1.innerHTML = link;
-	} else {
-		link1.innerHTML = linkName;
-	}
-
-	var c = classes[classIndex];
-	if(linkIndex == c.length){
-		c.push(link);
-	} else {
-		c[linkIndex] = link;
-	}
-
-	return 1;
+function aLink(parent, id, href, innerHTML){
+		var link = document.createElement("a");
+		link.href = href;
+		link.innerHTML = innerHTML;
+		link.id = "l" + id; //l11 or l21 or l31 ... first number increments by number of classes second increments by number of links in that class
+		parent.appendChild(link);
+		link.target="_blank";
+		return link;
 }
-
 
 /*
 * shows the classes array in an alert box
@@ -288,11 +271,55 @@ function show() {
 }
 
 /*
-* alerts the number of row in the table
+* allows the user to rename a class
 */
-function numberOfRows() {
-	var table = document.getElementById("maintable");
-	alert("table rows " + table.rows.length);
+function renameClass(){
+	var toChange = prompt("What is the class that you would like to rename");
+
+	if(classes[0][0] === "Generic Class"){
+		alert("Add a class first!");
+	}
+
+	for(var i = 0; i < classes.length; i++){
+		if(toChange === classes[i][0]){
+			var cname = prompt("What should be the new name of " + toChange + "?");
+			document.getElementById("p"+(i+1)).innerHTML = cname;
+			classes[i][0] = cname;
+		}
+	}
+	save();
+}
+
+/*
+* allows the user to rename a link 
+*/
+function renameLink(){
+	var linkName = prompt("Enter the name of your link");
+	for(var i = 0; i < localStorage.length; i++){
+		if(localStorage.getItem(localStorage.key(i)) === linkName){
+			var link = localStorage.key(i); //the actual link
+			//find the link in classes and change the value
+			for(var j = 0; j < classes.length; j++){
+				for(var z = 0; z < classes[j].length; z++){
+					if(classes[j][z] === link){
+						var newLink = prompt("What should the new link be?");
+						if(newLink != null){
+							classes[j][z] = newLink;
+							var linkElement = document.getElementById("l"+(j+1)+z);
+							linkElement.href = newLink;
+							var newName = prompt("What would you like to call this link?");
+							if(newName != null){
+								linkElement.innerHTML = newName;
+							} else {
+								linkElement.innerHTML = newLink;
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+	save();
 }
 
 /*
